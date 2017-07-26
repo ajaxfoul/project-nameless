2fba816daf3da835aeb6501cded1c64606f24910
Jacob <jacob.monrad.nygard@me.com>
2017-07-26 10:07:20 +0000

    new page + minor details

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,7 +7,12 @@ class PagesController < ApplicationController
   
   def about
     @random_number = rand(100)
+  end
+  
+  def random
+    random_texts = ["Eh", "Meh", "cent3r", "boi",]
     
+    @random_texts = random_texts.sample
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   get 'contact' => 'pages#contact'
   
     get 'about' => 'pages#about'
+    
+    get 'random' => 'pages#random'
+    
 end
